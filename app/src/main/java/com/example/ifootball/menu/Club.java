b0def94828850a61f0b49023ebfc9d02c68f4a6f
Quang Phạm <quangphuet99@gmail.com>
2020-04-22 15:23:48 +0700

    add new club

--- app/src/main/java/com/example/ifootball/menu/Club.java
+++ app/src/main/java/com/example/ifootball/menu/Club.java
@@ -2,17 +2,36 @@ package com.example.ifootball.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 import com.example.ifootball.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Club extends AppCompatActivity {
+
+    FloatingActionButton fabAddNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club);
+
+        fabAddNew = (FloatingActionButton) findViewById(R.id.fabClubAdd_AddNewClub);
+        fabAddNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+
+    public void openDialog() {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_add_new_club);
+        dialog.show();
     }
 
     @Override
