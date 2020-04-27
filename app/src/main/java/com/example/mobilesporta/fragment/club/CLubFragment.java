0b376dca97c35a1fd61732ff0cc6109382d56b8e
ngocthantk <ngocthanhotboy@gmail.com>
2020-04-27 11:28:11 +0700

    update-fragment-menu

--- app/src/main/java/com/example/mobilesporta/fragment/club/CLubFragment.java
+++ app/src/main/java/com/example/mobilesporta/fragment/club/CLubFragment.java
@@ -1,7 +1,12 @@
 package com.example.mobilesporta.fragment.club;
 
+import android.app.Dialog;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -10,11 +15,47 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.mobilesporta.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CLubFragment extends Fragment {
+
+    FloatingActionButton fabAddNew;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_club, container, false);
+        View view =  inflater.inflate(R.layout.fragment_club, container, false);
+        setHasOptionsMenu(true);
+        fabAddNew = (FloatingActionButton) view.findViewById(R.id.fabClubAdd_AddNewClub);
+        fabAddNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+
+        return view;
+    }
+
+    public void openDialog() {
+        Dialog dialog = new Dialog(this.getContext());
+        dialog.setContentView(R.layout.dialog_add_new_club);
+        dialog.show();
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.search_club_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //click item menu
+        return super.onOptionsItemSelected(item);
     }
 }
