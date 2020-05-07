facd52985ec2047bdaeb1e531e3f854e29183005
ngocthantk <ngocthanhotboy@gmail.com>
2020-05-08 00:25:47 +0700

    list-club

--- app/src/main/java/com/example/mobilesporta/data/service/ClubService.java
+++ app/src/main/java/com/example/mobilesporta/data/service/ClubService.java
@@ -3,16 +3,52 @@ package com.example.mobilesporta.data.service;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.mobilesporta.Home;
 import com.example.mobilesporta.model.ClubModel;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ClubService {
-    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    ArrayList<ClubModel> listClub = new ArrayList<>();
 
     public void addClub(ClubModel clubModel){
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
         mDatabase.child("clubs").push().setValue(clubModel);
+    }
+
+    public void getDataClub(String userId){
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("clubs");
+        Query infoClub = mDatabase.orderByChild("user_created_id").equalTo(userId);
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        ClubModel clubModel = snapshot.getValue(ClubModel.class);
+                        listClub.add(clubModel);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+
+        infoClub.addListenerForSingleValueEvent(valueEventListener);
     }
 }
