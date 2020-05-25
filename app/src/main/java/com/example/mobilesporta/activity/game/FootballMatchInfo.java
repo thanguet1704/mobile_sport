1286acd9ea688f14d7a16e6fefe2cd88110a36b7
Thắng Trịnh <ngocthanhotboy@gmail.com>
2020-05-25 11:31:04 +0700

    render-match-info-from-db

--- app/src/main/java/com/example/mobilesporta/activity/game/FootballMatchInfo.java
+++ app/src/main/java/com/example/mobilesporta/activity/game/FootballMatchInfo.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,11 +15,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mobilesporta.Home;
 import com.example.mobilesporta.R;
+import com.example.mobilesporta.activity.club.ClubProfile;
 import com.example.mobilesporta.data.service.ClubService;
 import com.example.mobilesporta.data.service.MatchService;
 import com.example.mobilesporta.model.ClubModel;
 import com.example.mobilesporta.model.MatchModel;
 import com.example.mobilesporta.model.StadiumModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -41,12 +45,16 @@ public class FootballMatchInfo extends AppCompatActivity {
     TextView tvDate, tvStadiumName, tvAddress, tvPhoneNumber, tvDescription, tvAmountsClub;
     Button btnSelectClub, btnRequest, btnBackToMatch, btnDeleteMatch;
     String stadiumName, stadiumAddress;
+    LinearLayout llHomeClub, llAwayClub;
 
     ClubService clubService = new ClubService();
     Map<String, ClubModel> mapClubs = clubService.getMapClubs();
 
     MatchService matchService = new MatchService();
     Map<String, MatchModel> mapMatchs = matchService.getMapMatchs();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String clubHomeId, clubAwayId;
+    String user_create_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +67,10 @@ public class FootballMatchInfo extends AppCompatActivity {
         stadiumAddress = intent.getStringExtra("stadium_address");
 
         connectView();
+
+        // hiển thị nút chọn đối nếu là người tạo match, tham gia nếu là người bắt kèo
+        displayButtonSelectClub(match_id);
+        //hiển thị thông tin kèo
         showMatchInfo(match_id);
 
         btnBackToMatch.setOnClickListener(new View.OnClickListener() {
@@ -93,6 +105,8 @@ public class FootballMatchInfo extends AppCompatActivity {
         btnRequest = findViewById(R.id.btn_request);
         btnBackToMatch = findViewById(R.id.btn_match_info_back);
         btnDeleteMatch = findViewById(R.id.btn_delete_match);
+        llHomeClub = findViewById(R.id.ll_home_club);
+        llAwayClub = findViewById(R.id.ll_away_club);
     }
 
     private ClubModel getClubById(String clubId, Map<String, ClubModel> map) {
@@ -145,12 +159,17 @@ public class FootballMatchInfo extends AppCompatActivity {
                     for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                         MatchModel matchModel = snapshot.getValue(MatchModel.class);
                         tvDate.setText(matchModel.getDate() + " từ " + matchModel.getTime());
-                        tvStadiumName.setText(stadiumName);
-                        tvAddress.setText(stadiumAddress);
+                        renderInfoStadium(matchModel.getStadium_id());
                         tvPhoneNumber.setText(matchModel.getPhone_number());
                         tvDescription.setText(matchModel.getDescription());
                         tvAmountsClub.setText("0" + " đội muốn tham gia trận đấu này");
+                        clubHomeId = matchModel.getClub_home_id();
+                        clubAwayId = matchModel.getClub_away_id();
                     }
+                    // hiển thị thông tin home club
+                    renderClubHome(clubHomeId);
+                    // hiển thị thông tin away club
+                    renderClubAway(clubAwayId);
                 }
             }
 
@@ -167,5 +186,121 @@ public class FootballMatchInfo extends AppCompatActivity {
         Intent intent = new Intent(FootballMatchInfo.this, Home.class);
         intent.putExtra("add_match", "true");
         startActivity(intent);
+    }
+
+    private void displayButtonSelectClub(String match_id){
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference("matchs");
+        database.orderByKey().equalTo(match_id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                        MatchModel matchModel = snapshot.getValue(MatchModel.class);
+                        if (user.getUid().equals(matchModel.getUser_created_id())){
+                            btnSelectClub.setVisibility(View.VISIBLE);
+                            btnRequest.setVisibility(View.GONE);
+                        }else{
+                            btnSelectClub.setVisibility(View.GONE);
+                            btnRequest.setVisibility(View.VISIBLE);
+                            btnDeleteMatch.setVisibility(View.GONE);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void renderClubHome(final String clubHomeId){
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference("clubs");
+        database.orderByKey().equalTo(clubHomeId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                        ClubModel clubModel = snapshot.getValue(ClubModel.class);
+                        Picasso.get().load(clubModel.getImage()).into(imgHomeClubName);
+                        txtHomeClubName.setText(clubModel.getClub_name());
+                        user_create_id = clubModel.getUser_created_id();
+                    }
+                    // click hiển thị thông tin club home
+                    llHomeClub.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(FootballMatchInfo.this, ClubProfile.class);
+                            intent.putExtra("club_id", clubHomeId);
+                            intent.putExtra("user_id", user_create_id);
+                            startActivity(intent);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void renderClubAway(final String clubAwayId){
+        if (clubAwayId.equals("")){
+            Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/mobilesporta-5bb33.appspot.com/o/image_club%2Fd63e35b31954db7a83f772702a348eb6.png?alt=media&token=964db416-1304-484c-88d0-c05aee101a1a").into(imgAwayClubName);
+        }else{
+            DatabaseReference database = FirebaseDatabase.getInstance().getReference("clubs");
+            database.orderByKey().equalTo(clubAwayId).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()){
+                        for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                            ClubModel clubModel = snapshot.getValue(ClubModel.class);
+                            Picasso.get().load(clubModel.getImage()).into(imgAwayClubName);
+                            txtAwayClubName.setText(clubModel.getClub_name());
+                            user_create_id = clubModel.getUser_created_id();
+                        }
+                        // click hiển thị thông tin club home
+                        llAwayClub.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(FootballMatchInfo.this, ClubProfile.class);
+                                intent.putExtra("club_id", clubAwayId);
+                                intent.putExtra("user_id", user_create_id);
+                                startActivity(intent);
+                            }
+                        });
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+    private void renderInfoStadium(String stadium_id){
+        final DatabaseReference database = FirebaseDatabase.getInstance().getReference("stadiums");
+        database.orderByKey().equalTo(stadium_id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        StadiumModel stadiumModel = snapshot.getValue(StadiumModel.class);
+                        tvStadiumName.setText(stadiumModel.getStadium_name());
+                        tvAddress.setText(stadiumModel.getAddress());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
